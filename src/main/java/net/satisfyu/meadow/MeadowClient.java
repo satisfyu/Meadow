ee4660bb7bda2b5bc2a7285ff58a3a1f181173b9
satisfyu <104036234+satisfyu@users.noreply.github.com>
2022-09-04 15:00:43 +0200

    initial setup

---
+++ src/main/java/net/satisfyu/meadow/MeadowClient.java
@@ -0,0 +1,10 @@
+package net.satisfyu.meadow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MeadowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
