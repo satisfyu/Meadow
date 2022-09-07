7d9ed12d770c636fcb2a889ea71b6622f574fed2
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2022-09-07 14:39:45 +0200

    beds, grass, crop

--- src/main/java/net/satisfyu/meadow/mixin/ExampleMixin.java
+++ src/main/java/net/satisfyu/meadow/mixin/ExampleMixin.java
@@ -1,16 +1,18 @@
 package net.satisfyu.meadow.mixin;
 
-import net.satisfyu.meadow.Meadow;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.block.entity.BedBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BedBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(BedBlockEntityRenderer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		Meadow.LOGGER.info("This line is printed by an example mod mixin!");
+	@Inject(at = @At("HEAD"),
+			method = "render(Lnet/minecraft/block/entity/BedBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V")
+	private void init(BedBlockEntity bedBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci) {
 	}
 }
