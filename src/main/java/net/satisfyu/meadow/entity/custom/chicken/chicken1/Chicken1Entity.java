60824bfad1821f57fe811d5994aca1d5f65cdc10
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2022-11-01 15:22:27 +0100

    some small changes

--- src/main/java/net/satisfyu/meadow/entity/custom/chicken/chicken1/Chicken1Entity.java
+++ src/main/java/net/satisfyu/meadow/entity/custom/chicken/chicken1/Chicken1Entity.java
@@ -2,10 +2,18 @@ package net.satisfyu.meadow.entity.custom.chicken.chicken1;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
+import net.satisfyu.meadow.entity.ModEntities;
 
 public class Chicken1Entity extends ChickenEntity {
     public Chicken1Entity(EntityType<? extends ChickenEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public Chicken1Entity createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
+        return ModEntities.CHICKEN1.create(serverWorld);
     }
 }
