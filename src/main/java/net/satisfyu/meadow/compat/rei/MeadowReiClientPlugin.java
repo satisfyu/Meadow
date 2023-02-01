741cf13a18c6736cf9f5c31d1b14b9a2edb1be3e
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-02-01 08:47:50 +0100

    all recipes done!

---
+++ src/main/java/net/satisfyu/meadow/compat/rei/MeadowReiClientPlugin.java
@@ -0,0 +1,48 @@
+package net.satisfyu.meadow.compat.rei;
+
+
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.Recipe;
+import net.satisfyu.meadow.block.ModBlocks;
+import net.satisfyu.meadow.compat.rei.cooking.CookingPotCategory;
+import net.satisfyu.meadow.compat.rei.cooking.CookingPotDisplay;
+import net.satisfyu.meadow.recipes.pot.CookingPotRecipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeadowReiClientPlugin implements REIClientPlugin {
+
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new CookingPotCategory());
+
+
+        registry.addWorkstations(CookingPotDisplay.COOKING_POT_DISPLAY, EntryStacks.of(ModBlocks.COOKING_POT));
+
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.registerFiller(CookingPotRecipe.class, CookingPotDisplay::new);
+
+    }
+
+
+
+    public static List<Ingredient> ingredients(Recipe<Inventory> recipe, ItemStack stack){
+        List<Ingredient> l = new ArrayList<>(recipe.getIngredients());
+        l.add(0, Ingredient.ofItems(stack.getItem()));
+        return l;
+    }
+
+}
---
+++ src/main/java/net/satisfyu/meadow/compat/rei/cooking/CookingPotDisplay.java
@@ -0,0 +1,46 @@
+package net.satisfyu.meadow.compat.rei.cooking;
+
+
+import net.satisfyu.meadow.Meadow;
+import net.satisfyu.meadow.compat.rei.MeadowReiClientPlugin;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.util.Identifier;
+import net.satisfyu.meadow.recipes.pot.CookingPotRecipe;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class CookingPotDisplay extends BasicDisplay {
+
+    public static final CategoryIdentifier<CookingPotDisplay> COOKING_POT_DISPLAY = CategoryIdentifier.of(Meadow.MOD_ID, "cooking_pot_display");
+
+
+    public CookingPotDisplay(Recipe<Inventory> recipe) {
+        this(EntryIngredients.ofIngredients(MeadowReiClientPlugin.ingredients(recipe, getContainer(recipe))), Collections.singletonList(EntryIngredients.of(recipe.getOutput())), Optional.ofNullable(recipe.getId()));
+    }
+
+    public CookingPotDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs, Optional<Identifier> location) {
+        super(inputs, outputs, location);
+    }
+
+
+    @Override
+    public CategoryIdentifier<?> getCategoryIdentifier() {
+        return COOKING_POT_DISPLAY;
+    }
+
+    public static ItemStack getContainer(Recipe<Inventory> recipe){
+        if(recipe instanceof CookingPotRecipe c){
+            return c.getContainer();
+        }
+        else return ItemStack.EMPTY;
+    }
+
+}
