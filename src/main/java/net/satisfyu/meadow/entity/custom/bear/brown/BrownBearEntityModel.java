90883135f70ba23ab3a20637fcde0f7317d5cea3
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-02-11 15:03:46 +0100

    brown bear model change

--- src/main/java/net/satisfyu/meadow/entity/custom/bear/brown/BrownBearEntityModel.java
+++ src/main/java/net/satisfyu/meadow/entity/custom/bear/brown/BrownBearEntityModel.java
@@ -18,12 +18,23 @@ public class BrownBearEntityModel extends PolarBearEntityModel<BrownBearEntity> 
 		.uv(0, 37).mirrored().cuboid(3.5F, -4.0F, -1.0F, 2.0F, 2.0F, 1.0F, new Dilation(0.0F)).mirrored(false)
 		.uv(12, 2).cuboid(-1.5F, -3.0F, 29.0F, 3.0F, 3.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 10.0F, -16.0F));
 
+
+		modelPartData.addChild("right_hind_leg", ModelPartBuilder.create().uv(50, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(-4.5F, 14.0F, 6.0F));
+
+		modelPartData.addChild("left_hind_leg", ModelPartBuilder.create().uv(33, 37).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(4.5F, 14.0F, 6.0F));
+
+		modelPartData.addChild("right_front_leg", ModelPartBuilder.create().uv(51, 17).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.5F, 14.0F, -8.0F));
+
+		modelPartData.addChild("left_front_leg", ModelPartBuilder.create().uv(51, 0).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(3.5F, 14.0F, -8.0F));
+		/*
 		ModelPartBuilder modelPartBuilder = ModelPartBuilder.create().uv(50, 22).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 8.0F);
 		modelPartData.addChild("right_hind_leg", modelPartBuilder, ModelTransform.pivot(-4.5F, 14.0F, 6.0F));
 		modelPartData.addChild("left_hind_leg", modelPartBuilder, ModelTransform.pivot(4.5F, 14.0F, 6.0F));
 		ModelPartBuilder modelPartBuilder2 = ModelPartBuilder.create().uv(50, 40).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 10.0F, 6.0F);
 		modelPartData.addChild("right_front_leg", modelPartBuilder2, ModelTransform.pivot(-3.5F, 14.0F, -8.0F));
 		modelPartData.addChild("left_front_leg", modelPartBuilder2, ModelTransform.pivot(3.5F, 14.0F, -8.0F));
+
+		 */
 
 		return TexturedModelData.of(modelData, 128, 128);
 	}
