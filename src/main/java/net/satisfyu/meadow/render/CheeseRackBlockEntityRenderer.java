ab87b98241a2733a225d0bcf6213ed860549f2cf
ANAR <qarabala4732@gmail.com>
2023-02-18 13:33:18 +0400

    you can place and get and cheese instead of 1st first and then 2nd
    Took 40 minutes

--- src/main/java/net/satisfyu/meadow/render/CheeseRackBlockEntityRenderer.java
+++ src/main/java/net/satisfyu/meadow/render/CheeseRackBlockEntityRenderer.java
@@ -6,12 +6,9 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.util.math.Vec3f;
 import net.satisfyu.meadow.block.cheeseRack.CheeseRackBlock;
 import net.satisfyu.meadow.block.cheeseRack.CheeseRackBlockEntity;
-
-import java.util.List;
 
 import static net.satisfyu.meadow.util.ClientUtil.renderBlock;
 
@@ -26,18 +23,17 @@ public class CheeseRackBlockEntityRenderer implements BlockEntityRenderer<Cheese
         }
         BlockState selfState = entity.getCachedState();
         if (selfState.getBlock() instanceof CheeseRackBlock) {
-            List<Item> items = entity.getItems();
             matrices.push();
             applyBlockAngle(matrices, selfState, 180);
-            if (!items.isEmpty()) {
-                BlockState state = ((BlockItem) items.get(0)).getBlock().getDefaultState();
-                matrices.translate(-0.5f, 0.1f, -0.5f);
+            matrices.translate(-0.5f, 0.1f, -0.5f);
+            if (entity.hasStack(0)) {
+                BlockState state = ((BlockItem) entity.getStack(0).getItem()).getBlock().getDefaultState();
                 renderBlock(state, matrices, vertexConsumers, entity);
-                if (items.size() > 1) {
-                    state = ((BlockItem) items.get(1)).getBlock().getDefaultState();
-                    matrices.translate(0f, 0.4f, 0f);
-                    renderBlock(state, matrices, vertexConsumers, entity);
-                }
+            }
+            matrices.translate(0f, 0.4f, 0f);
+            if (entity.hasStack(1)) {
+                BlockState state = ((BlockItem) entity.getStack(1).getItem()).getBlock().getDefaultState();
+                renderBlock(state, matrices, vertexConsumers, entity);
             }
             matrices.pop();
         }
