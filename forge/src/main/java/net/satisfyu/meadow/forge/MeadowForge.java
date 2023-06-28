ffc846eec7bfdb7ff9e58b6b46be0683e87e5f5e
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-06-28 15:14:52 +0200

    spawn registration fix

--- forge/src/main/java/net/satisfyu/meadow/forge/MeadowForge.java
+++ forge/src/main/java/net/satisfyu/meadow/forge/MeadowForge.java
@@ -1,12 +1,17 @@
 package net.satisfyu.meadow.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.Heightmap;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.satisfyu.meadow.Meadow;
-import net.satisfyu.meadow.forge.world.ForgeEntitySpawn;
+import net.satisfyu.meadow.registry.EntityRegistry;
 import net.satisfyu.meadow.terrablender.MeadowRegion;
 
 @Mod(Meadow.MOD_ID)
@@ -25,9 +30,102 @@ public class MeadowForge {
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork( () -> {
             MeadowRegion.loadTerrablender();
-            ForgeEntitySpawn.registerEntitySpawn();
         });
         Meadow.commonSetup();
+    }
 
+    @SubscribeEvent
+    public static void spawnEvent(SpawnPlacementRegisterEvent event){
+        event.register(EntityRegistry.BROWN_BEAR.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.DAIRY_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.COOKIE_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.FUZZY_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.HIGHLAND_CATTLE.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.MEADOW_CHICKEN.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.FLECKED_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.SUNSET_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.ALBINO_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.CREAM_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.PINTO_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.WATER_BUFFALO.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.HORNED_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.ROCKY_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.DARK_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.UMBRA_COW.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+
+        event.register(EntityRegistry.INKY_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
+        event.register(EntityRegistry.PATCHED_SHEEP.get(),
+                SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                AnimalEntity::isValidNaturalSpawn,
+                SpawnPlacementRegisterEvent.Operation.AND
+                );
     }
 }
