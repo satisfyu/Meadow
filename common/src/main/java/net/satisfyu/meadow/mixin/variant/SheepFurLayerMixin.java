b4facc787f54cd32a7d8b2b94109300eda056273
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-08-10 14:49:49 +0200

    forge crash fix and jei runtime remove

--- common/src/main/java/net/satisfyu/meadow/mixin/variant/SheepFurLayerMixin.java
+++ common/src/main/java/net/satisfyu/meadow/mixin/variant/SheepFurLayerMixin.java
@@ -1,18 +1,32 @@
 package net.satisfyu.meadow.mixin.variant;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.layers.SheepFurLayer;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.satisfyu.meadow.entity.sheep.SheepVar;
 import net.satisfyu.meadow.util.MeadowIdentifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(SheepFurLayer.class)
 public class SheepFurLayerMixin {
 
+    // Does not work because ModifyArgs is bugged on forge
+
+    /*
     @ModifyArgs(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/animal/Sheep;FFFFFF)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/SheepFurLayer;coloredCutoutModelCopyLayerRender(Lnet/minecraft/client/model/EntityModel;Lnet/minecraft/client/model/EntityModel;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFFFFF)V"))
     private void onGetTexture(Args args) {
@@ -24,4 +38,21 @@ public class SheepFurLayerMixin {
             args.set(2, new MeadowIdentifier(String.format("textures/entity/sheep/%s_sheep_fur.png", var.getSerializedName())));
         }
     }
+     */
+
+    // Accesswidening SHEEP_FUR_LOCATION, so it can be changed before the render
+    @Unique
+    private static final ResourceLocation SHEEP_FUR_LOCATION = new ResourceLocation("textures/entity/sheep/sheep_fur.png");
+    @Inject(
+            method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/animal/Sheep;FFFFFF)V",
+            at = @At(value = "HEAD"))
+    public void onRender(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, Sheep sheep, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+        SheepVar var = SheepVar.getVariant(sheep);
+        if(var.equals(SheepVar.DEFAULT) || var.equals(SheepVar.HORNED)){
+            SheepFurLayer.SHEEP_FUR_LOCATION = SHEEP_FUR_LOCATION;
+            return;
+        }
+        SheepFurLayer.SHEEP_FUR_LOCATION = new MeadowIdentifier(String.format("textures/entity/sheep/%s_sheep_fur.png", var.getSerializedName()));
+    }
+
 }
