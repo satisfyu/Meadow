4142a5ad343e2f51c03728e02feee8d4d3df597c
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-05-07 20:00:09 +0200

    Fixed: NPE when coming to a Meadow Village

--- common/src/main/java/net/satisfyu/meadow/client/render/block/storage/CheeseRackRenderer.java
+++ common/src/main/java/net/satisfyu/meadow/client/render/block/storage/CheeseRackRenderer.java
@@ -13,16 +13,18 @@ public class CheeseRackRenderer implements StorageTypeRenderer {
     @Override
     public void render(StorageBlockEntity storageBlockEntity, PoseStack poseStack, MultiBufferSource multiBufferSource, NonNullList<ItemStack> nonNullList) {
         poseStack.translate(-0.5f, 0.05f, -0.5f);
-
-        ItemStack itemStack1 = nonNullList.get(0);
-        if(itemStack1.getItem() instanceof BlockItem blockItem){
-            ClientUtil.renderBlockFromItem(blockItem, poseStack, multiBufferSource, storageBlockEntity);
+        if (!nonNullList.isEmpty()) {
+            ItemStack itemStack1 = nonNullList.get(0);
+            if(itemStack1.getItem() instanceof BlockItem blockItem){
+                ClientUtil.renderBlockFromItem(blockItem, poseStack, multiBufferSource, storageBlockEntity);
+            }
         }
-
-        ItemStack itemStack2 = nonNullList.get(1);
-        if(itemStack2.getItem() instanceof BlockItem blockItem){
-            poseStack.translate(0f, 0.4f, 0f);
-            ClientUtil.renderBlockFromItem(blockItem, poseStack, multiBufferSource, storageBlockEntity);
+        if (nonNullList.size() > 1) {
+            ItemStack itemStack2 = nonNullList.get(1);
+            if(itemStack2.getItem() instanceof BlockItem blockItem){
+                poseStack.translate(0f, 0.4f, 0f);
+                ClientUtil.renderBlockFromItem(blockItem, poseStack, multiBufferSource, storageBlockEntity);
+            }
         }
     }
 }
